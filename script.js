1054c54e5bdf00a1cbea98a65041b2e2b8835668
Calvin Beardemphl <your.gr8.pal.cal@gmail.com>
2024-03-04 22:46:34 -0800

    1 solid hour of work down

--- script.js
+++
@@ -1,5 +0,0 @@
-function toggleCard(card) {
-    const content = card.querySelector('.card-content');
-
-    content.style.maxHeight = content.style.maxHeight ? null : content.scrollHeight + 'px';
-}
